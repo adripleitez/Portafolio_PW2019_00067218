58cb1c4052d76a86dc5c0d09c423120649fcdfa8
adripleitez <00067218@uca.edu.sv>
2019-09-23 11:09:05 -0600

    [Add]: ejer 9

---
+++ Laboratorios/Laboratorio5/js/ejercicio9.js
@@ -0,0 +1,10 @@
+function radio(r){
+    area = 3.14*Math.pow(r,2)
+   
+   if(r < 0){
+       console.log(-1);
+   }else{
+        console.log("El area de la circunferencia es" +" "+  area);
+   }
+   
+}

a5a0c8b5e07c9933a6477b04b4f07c6fff3af424
adripleitez <00067218@uca.edu.sv>
2019-11-04 21:54:24 -0600

    fixing uri

--- Laboratorios/Labo10/bin/database.js
+++ Laboratorios/Labo10/bin/database.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let database = 'RegistroAPI';
+let database = 'Labo10';
 let port = '27017';
 let host = 'localhost';
 let uri = `mongodb://${host}:${port}/${database}`;
 
 const connectDB = () => {
 	mongoose
-		.connect(uri, { useNewUrlParser: true})
+		.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
 		.then(() => {
 			console.log('The conection was succesfull.');
 		})
